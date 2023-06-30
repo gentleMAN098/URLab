b0974970a30912d422160d8f70fb94800a8f66c2
hsnards <1214hassan1214@gmail.com>
2023-06-30 19:24:36 +0330

    feat: add useSelectableItems hook , add subscribe to mainmenu store, add usage of the hook in app.js

--- admin/src/hooks/useMainMenu.jsx
+++ admin/src/hooks/useMainMenu.jsx
@@ -1,27 +1,30 @@
 import { create } from 'zustand';
 import { get, update } from 'idb-keyval';
-
-const useMainMenuStore = create( ( set ) => ( {
+import { subscribeWithSelector } from "zustand/middleware"
+const useMainMenuStore = create(subscribeWithSelector((set) => ({
 	activePage: 'urlslab-modules',
-	setPage: ( activePage ) => set( ( { activePage } ) ),
-} ) );
+	setPage: (activePage) => set(({ activePage })),
+})));
+
+
+ useMainMenuStore.subscribe((state) => state.activePage, console.log)
 
 export default function useMainMenu() {
-	const activePage = useMainMenuStore( ( state ) => state.activePage );
-	const setPage = useMainMenuStore( ( state ) => state.setPage );
+	const activePage = useMainMenuStore((state) => state.activePage);
+	const setPage = useMainMenuStore((state) => state.setPage);
 
-	const setActivePage = ( moduleId ) => {
-		setPage( moduleId );
-		update( 'activePage', () => {
+	const setActivePage = (moduleId) => {
+		setPage(moduleId);
+		update('activePage', () => {
 			return moduleId;
-		} );
+		});
 	};
 
 	const getActivePage = async () => {
-		const savedPage = await get( 'activePage' );
+		const savedPage = await get('activePage');
 
-		if ( savedPage ) {
-			setPage( savedPage );
+		if (savedPage) {
+			setPage(savedPage);
 			return savedPage;
 		}
 		return 'urlslab-modules';
