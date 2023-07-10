dd3bb79130529a2f64e2680f9b5fe7c4ec41956a
parsaGhorbanian <cameronzigzal.com@gmail.com>
2023-07-10 14:42:30 +0330

    roles added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,22 @@
-import { useMemo, useState, Suspense, useEffect } from "react";
+import { useMemo, useState, Suspense, useEffect } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { update } from "idb-keyval";
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { update } from 'idb-keyval';
 
-import { getFetch, postFetch } from "./api/fetching";
-import { fetchSettings } from "./api/settings";
-import { fetchLangs } from "./api/fetchLangs";
+import { getFetch, postFetch } from './api/fetching';
+import { fetchSettings } from './api/settings';
+import { fetchLangs } from './api/fetchLangs';
 
-import hexToHSL from "./lib/hexToHSL";
+import hexToHSL from './lib/hexToHSL';
 
-import Notifications from "./components/Notifications";
-import MainMenu from "./components/MainMenu";
-import DynamicModule from "./components/DynamicModule";
-import Header from "./components/Header";
+import Notifications from './components/Notifications';
+import MainMenu from './components/MainMenu';
+import DynamicModule from './components/DynamicModule';
+import Header from './components/Header';
 
-import "./assets/styles/style.scss";
+import './assets/styles/style.scss';
 
-import { useSelectableItems } from "./hooks/useSelectableItems";
+import { useSelectableItems } from './hooks/useSelectableItems';
 
 export default function App() {
   const queryClient = useQueryClient();
@@ -24,29 +24,27 @@ export default function App() {
 
   useEffect(() => {
     if (prefetch) {
-      update("apiKeySet", () => true);
+      update('apiKeySet', () => true);
       // Checking if API is set in advance
       async function getApiKey() {
         const generalData = await queryClient.fetchQuery({
-          queryKey: ["general"],
-          queryFn: () => fetchSettings("general").then((data) => data),
+          queryKey: ['general'],
+          queryFn: () => fetchSettings('general').then((data) => data),
           refetchOnWindowFocus: false,
         });
 
-        const isApiObject = generalData?.filter(
-          (dataset) => dataset.id === "api"
-        )[0];
-        const hasApiKey = isApiObject?.options["urlslab-api-key"].value;
+        const isApiObject = generalData?.filter((dataset) => dataset.id === 'api')[0];
+        const hasApiKey = isApiObject?.options['urlslab-api-key'].value;
 
         if (!hasApiKey) {
-          update("apiKeySet", () => false);
+          update('apiKeySet', () => false);
         }
       }
       getApiKey();
 
       // Creating languages query object in advance
       queryClient.prefetchQuery({
-        queryKey: ["languages"],
+        queryKey: ['languages'],
         queryFn: async () => await fetchLangs(),
         refetchOnWindowFocus: false,
       });
@@ -54,7 +52,7 @@ export default function App() {
       /* Creating all endpoints query object in advance
 			to check for allowed+required import/insert/edit CSV fields */
       queryClient.prefetchQuery({
-        queryKey: ["routes"],
+        queryKey: ['routes'],
         queryFn: async () => {
           const response = await getFetch();
           if (response.ok) {
@@ -66,14 +64,14 @@ export default function App() {
 
       // Creating Tags/Labels query object in advance
       queryClient.prefetchQuery({
-        queryKey: ["label", "menu"],
+        queryKey: ['label', 'menu'],
         queryFn: async () => {
-          const tags = await postFetch("label", { rows_per_page: 500 });
+          const tags = await postFetch('label', { rows_per_page: 500 });
           const tagsArray = await tags.json();
           tagsArray?.map((tag) => {
             const { lightness } = hexToHSL(tag.bgcolor);
             if (lightness < 70) {
-              return (tag.className = "dark");
+              return (tag.className = 'dark');
             }
             return tag;
           });
@@ -84,9 +82,9 @@ export default function App() {
 
       // Creating Tags/Labels query object in advance
       queryClient.prefetchQuery({
-        queryKey: ["label", "modules"],
+        queryKey: ['label', 'modules'],
         queryFn: async () => {
-          const response = await getFetch("label/modules");
+          const response = await getFetch('label/modules');
           if (response.ok) {
             return response.json();
           }
@@ -99,12 +97,10 @@ export default function App() {
   }, []);
 
   const { data } = useQuery({
-    queryKey: ["modules"],
+    queryKey: ['modules'],
     queryFn: async () => {
       if (prefetch) {
-        const response = await getFetch("module").then(
-          (ModuleData) => ModuleData
-        );
+        const response = await getFetch('module').then((ModuleData) => ModuleData);
         if (response.ok) {
           return response.json();
         }
@@ -117,58 +113,18 @@ export default function App() {
     delete data?.general;
     return data;
   }, [data]);
-  const {
-    items,
-    toggleSelectItem,
-    toggleSelectAllItems,
-    isAllSelected,
-    selectedItems,
-  } = useSelectableItems({
-    initialItems: [
-      { id: 1, title: "hasan" },
-      { id: 2, title: "mmdzi" },
-    ],
-  });
+
   return (
     <div className="urlslab-app flex">
       {fetchedModules && (
         <Suspense>
-          <MainMenu
-            modules={!fetchedModules || Object.values(fetchedModules)}
-          />
+          <MainMenu modules={!fetchedModules || Object.values(fetchedModules)} />
         </Suspense>
       )}
 
       <div className="urlslab-app-main">
-        <label htmlFor="all">
-          selectAll
-          <input
-            name="all"
-            type="checkbox"
-            checked={isAllSelected}
-            onChange={() => toggleSelectAllItems()}
-          />
-        </label>
-        {items.map((item) => (
-          <label htmlFor={item.title}>
-            {item.title}
-            <input
-              name={item.title}
-              type="checkbox"
-              checked={item.isSelected}
-              onChange={() => toggleSelectItem(item)}
-            />
-          </label>
-        ))}
         <Header fetchedModules={fetchedModules} />
-
-        <h2>selected Items</h2>
-        {selectedItems.map((item) => (
-          <p>{item.title}</p>
-        ))}
-        <DynamicModule
-          modules={!fetchedModules || Object.values(fetchedModules)}
-        />
+        <DynamicModule modules={!fetchedModules || Object.values(fetchedModules)} />
       </div>
       <Notifications />
     </div>
--- admin/src/hooks/useSelectableItems.jsx
+++ admin/src/hooks/useSelectableItems.jsx
@@ -1,11 +1,15 @@
-import { useCallback, useEffect, useMemo } from "react";
-import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
+import { useCallback, useEffect, useMemo } from 'react';
+import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
 
 const useSelectItemsStore = create(
   subscribeWithSelector((set) => ({
     items: [],
-    identifierKey: "id",
+    identifierKey: 'id',
+    setIdentifierKey: (key) =>
+      set({
+        identifierKey: key,
+      }),
     initItems: (initialItems) =>
       set({
         items: initialItems.map((item) => ({
@@ -16,9 +20,7 @@ const useSelectItemsStore = create(
     toggleItem: (identifierValue) =>
       set((state) => {
         const { items, identifierKey } = state;
-        const itemIndex = items.findIndex(
-          (itemFound) => itemFound[identifierKey] === identifierValue
-        );
+        const itemIndex = items.findIndex((itemFound) => itemFound[identifierKey] === identifierValue);
         const item = items[itemIndex];
         const updatedItems = [...items];
         updatedItems.splice(itemIndex, 1, {
@@ -30,9 +32,7 @@ const useSelectItemsStore = create(
     toggleAllItems: () =>
       set((state) => {
         const { items } = state;
-        const isAllUnselected = items.every(
-          (item) => item.isSelected === false
-        );
+        const isAllUnselected = items.every((item) => item.isSelected === false);
         const newItems = items.map((item) => ({
           ...item,
           isSelected: isAllUnselected,
@@ -44,27 +44,25 @@ const useSelectItemsStore = create(
 
 useSelectItemsStore.subscribe((state) => state.items, console.log);
 
-export const useSelectableItems = ({ initialItems, identifierKey = "id" }) => {
-  const { initItems, toggleItem, items, toggleAllItems } = useSelectItemsStore(
-    (state) => ({
-      initItems: state.initItems,
-      toggleItem: state.toggleItem,
-      items: state.items,
-      toggleAllItems: state.toggleAllItems,
-    })
-  );
+export const useSelectableItems = ({ initialItems, identifierKey = 'id' }) => {
+  const { initItems, toggleItem, items, toggleAllItems, setIdentifierKey } = useSelectItemsStore((state) => ({
+    initItems: state.initItems,
+    toggleItem: state.toggleItem,
+    items: state.items,
+    toggleAllItems: state.toggleAllItems,
+    setIdentifierKey: state.setIdentifierKey,
+  }));
 
   //initial items and set isSelected property to anyone
   useEffect(() => {
     const shouldInitializeItems = (initialItems ?? []).length > 0;
-    const hasItems = items.length > 0;
-
-    if (!shouldInitializeItems || hasItems) {
+    // const hasItems = items.length > 0;
+    if (!shouldInitializeItems) {
       return;
     }
-
+    setIdentifierKey(identifierKey);
     initItems(initialItems);
-  }, [items, initialItems]);
+  }, [initialItems]);
 
   // toggle select single item handler
   const toggleSelectItem = useCallback(
@@ -91,10 +89,7 @@ export const useSelectableItems = ({ initialItems, identifierKey = "id" }) => {
   );
 
   //is All Items Selected
-  const isAllSelected = useMemo(
-    () => selectedItems.length === items.length,
-    [selectedItems, items]
-  );
+  const isAllSelected = useMemo(() => selectedItems.length === items.length, [selectedItems, items]);
 
   const result = useMemo(
     () => ({
@@ -104,13 +99,7 @@ export const useSelectableItems = ({ initialItems, identifierKey = "id" }) => {
       toggleSelectItem,
       isAllSelected,
     }),
-    [
-      items,
-      selectedItems,
-      toggleSelectAllItems,
-      toggleSelectItem,
-      isAllSelected,
-    ]
+    [items, selectedItems, toggleSelectAllItems, toggleSelectItem, isAllSelected]
   );
 
   return result;
