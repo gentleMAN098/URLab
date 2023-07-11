5aea72e97f85a6964a15bf855c0bb6d415a05ea0
hsnards <1214hassan1214@gmail.com>
2023-07-11 21:54:22 +0330

    fix: initial rowSelection of  rolesTable

--- admin/src/tables/roles/RolesTable.jsx
+++ admin/src/tables/roles/RolesTable.jsx
@@ -1,52 +1,56 @@
-import { useEffect, useMemo, useState } from 'react';
-import { getFetch } from '../../api/fetching';
-import ModuleViewHeaderBottom from '../../components/ModuleViewHeaderBottom';
-import Table from '../../components/TableComponent';
-import { createColumnHelper } from '@tanstack/react-table';
-import { startCase } from '../../lib/startCase';
-import SearchField from '../../elements/SearchField';
-import { useI18n } from '@wordpress/react-i18n';
-import ModuleViewHeader from '../../components/ModuleViewHeader';
-import Button from '../../elements/Button';
-import { ReactComponent as PlusIcon } from '../../assets/images/icons/icon-plus.svg';
-import Checkbox from '../../elements/Checkbox';
-import InputField from '../../elements/InputField';
-import { useSelectableItems } from '../../hooks/useSelectableItems';
-import { useQuery } from '@tanstack/react-query';
-import AllCapabilities from '../../components/AllCapabilities';
-import SelectedCapabilities from '../../components/SelectedCapabilities';
-import ActionsBar from '../../components/ActionsBar';
-import EditRole from '../../components/EditRole';
+import { useEffect, useMemo, useState } from "react";
+import { getFetch } from "../../api/fetching";
+import ModuleViewHeaderBottom from "../../components/ModuleViewHeaderBottom";
+import Table from "../../components/TableComponent";
+import { createColumnHelper } from "@tanstack/react-table";
+import { startCase } from "../../lib/startCase";
+import SearchField from "../../elements/SearchField";
+import { useI18n } from "@wordpress/react-i18n";
+import ModuleViewHeader from "../../components/ModuleViewHeader";
+import Button from "../../elements/Button";
+import { ReactComponent as PlusIcon } from "../../assets/images/icons/icon-plus.svg";
+import Checkbox from "../../elements/Checkbox";
+import InputField from "../../elements/InputField";
+import { useSelectableItems } from "../../hooks/useSelectableItems";
+import { useQuery } from "@tanstack/react-query";
+import AllCapabilities from "../../components/AllCapabilities";
+import SelectedCapabilities from "../../components/SelectedCapabilities";
+import ActionsBar from "../../components/ActionsBar";
+import EditRole from "../../components/EditRole";
 
 //constant
-const roleSlug = 'permission/role';
-const userSlug = 'permission/user';
-const capSlug = 'permission/capability';
+const roleSlug = "permission/role";
+const userSlug = "permission/user";
+const capSlug = "permission/capability";
 
 const header = {
-  name: 'Name',
-  modules: 'Allowed',
+  name: "Name",
+  modules: "Allowed",
 };
 
-const tableMenu = new Map([['Capabilities', 'Users']]);
+const tableMenu = new Map([["Capabilities", "Users"]]);
 const columnHelper = createColumnHelper();
-const columns = [columnHelper.accessor('Role'), columnHelper.accessor('Capabilities'), columnHelper.accessor('User')];
+const columns = [
+  columnHelper.accessor("Role"),
+  columnHelper.accessor("Capabilities"),
+  columnHelper.accessor("User"),
+];
 
 export default function RolesTable() {
-  const [activeSection, setActiveSection] = useState('capabilities');
+  const [activeSection, setActiveSection] = useState("capabilities");
   const [selectedRowData, setSelectedRowData] = useState({});
   const [filterBy, setFilterBy] = useState({});
   const [caps, setCaps] = useState([]);
   const { items, toggleSelectItem } = useSelectableItems({
-    identifierKey: 'capabilitySlug',
+    identifierKey: "capabilitySlug",
     initialItems: selectedRowData.CapabilitiesItem,
   });
   const { data: rolesData, isLoading: rolesLoading } = useQuery({
-    queryKey: ['roles'],
+    queryKey: ["roles"],
     queryFn: async () => {
       const roleRes = await getFetch(roleSlug);
       const roleData = await roleRes.json();
-      console.log('role', roleData);
+      console.log("role", roleData);
       return roleData;
     },
     refetchOnWindowFocus: false,
@@ -54,7 +58,7 @@ export default function RolesTable() {
 
   // console.log("items", items);
   const { data: usersData, isLoading: usersLoading } = useQuery({
-    queryKey: ['users'],
+    queryKey: ["users"],
     queryFn: async () => {
       const userRes = await getFetch(userSlug);
       const userData = await userRes.json();
@@ -64,9 +68,8 @@ export default function RolesTable() {
   });
 
   const normalizeTableData = useMemo(() => {
-    console.log({ rolesData, usersData }, 'run');
     if (rolesData && usersData) {
-      return rolesData.reduce((acc, cur) => {
+      const data = rolesData.reduce((acc, cur) => {
         acc = [
           ...acc,
           {
@@ -74,21 +77,26 @@ export default function RolesTable() {
             Capabilities: Object.keys(cur.role.capabilities).length,
             User: usersData.filter((it) => it.roles[0] === cur.role_key).length,
             RoleKey: cur.role_key,
-            CapabilitiesItem: Object.keys(cur.role.capabilities).reduce((accCap, curCap) => {
-              accCap = [
-                ...accCap,
-                {
-                  capabilityName: startCase(curCap),
-                  isSelected: cur.role.capabilities[curCap],
-                  capabilitySlug: curCap,
-                },
-              ];
-              return accCap;
-            }, []),
+            CapabilitiesItem: Object.keys(cur.role.capabilities).reduce(
+              (accCap, curCap) => {
+                accCap = [
+                  ...accCap,
+                  {
+                    capabilityName: startCase(curCap),
+                    isSelected: cur.role.capabilities[curCap],
+                    capabilitySlug: curCap,
+                  },
+                ];
+                return accCap;
+              },
+              []
+            ),
           },
         ];
         return acc;
       }, []);
+      setSelectedRowData(data[0]);
+      return data;
     } else return [];
   }, [rolesData, usersData]);
 
@@ -96,7 +104,13 @@ export default function RolesTable() {
     const capRes = await getFetch(capSlug);
     const capData = await capRes.json();
     const normalizeCapList = capData.reduce((acc, cur) => {
-      acc = [...acc, { capabilityName: startCase(cur.capability), capabilitySlug: cur.capability }];
+      acc = [
+        ...acc,
+        {
+          capabilityName: startCase(cur.capability),
+          capabilitySlug: cur.capability,
+        },
+      ];
       return acc;
     }, []);
 
@@ -109,7 +123,7 @@ export default function RolesTable() {
 
   const handleFiltering = ({ input, type }) => {
     let inputValue = input;
-    if (typeof input === 'string') {
+    if (typeof input === "string") {
       inputValue = input.toLowerCase();
     }
 
@@ -117,18 +131,17 @@ export default function RolesTable() {
       return { ...filter, [type]: inputValue };
     });
   };
-  
-  
+
   /////////////////// how to set this data?? /////////////
   useEffect(() => {
- console.log('selected row:', selectedRowData?.id ?? null );
-//  console.log(rows[0].original);
-  }, [])
-  
-const initialSelectedRow = (rows) => {
-  setSelectedRowData(rows[0].original);
-  rows[0].getIsSelected();
-}
+    console.log("selected row:", selectedRowData?.id ?? null);
+    //  console.log(rows[0].original);
+  }, []);
+
+  const initialSelectedRow = (rows) => {
+    setSelectedRowData(rows[0].original);
+    rows[0].getIsSelected();
+  };
   return (
     <>
       <ModuleViewHeaderBottom
@@ -138,44 +151,46 @@ const initialSelectedRow = (rows) => {
         noFiltering
         noCount
         hideActions
-        options={{ header, notWide: true, title: 'Add new role', id: 'name' }}
+        options={{ header, notWide: true, title: "Add new role", id: "name" }}
       />
       <div className="roles-layout">
-        <Table
-          className="fadeInto"
-          slug={roleSlug}
-          columns={columns}
-          data={!rolesLoading ? normalizeTableData : []}
-          getRowExtraProps={(row, rows) =>{
-            return {
-              onClick: (e) => {
-                console.log('row', row, 'rows', rows);
-                row.toggleSelected();
-                setSelectedRowData(row.original);
-                rows.map((item) => {
-                  if (item.getIsSelected() && item.id !== row.id) {
-                    item.toggleSelected();
-                  }
-                });
-              },
-            };
-          }} 
-          // ({
-          //   /////////////???????/////
-          //   // initialState: {()},
-          //   // onLoad: ?????,
-          //   onClick: (e) => {
-          //     console.log('row', row, 'rows', rows);
-          //     row.toggleSelected();
-          //     setSelectedRowData(row.original);
-          //       rows.map((item) => {
-          //         if (item.getIsSelected() && item.id !== row.id) {
-          //           item.toggleSelected();
-          //         }
-          //       });
-          //     },
-          // })}
-        />
+        {rolesData && usersData ? (
+          <Table
+            className="fadeInto"
+            slug={roleSlug}
+            columns={columns}
+            data={!rolesLoading ? normalizeTableData : []}
+            getRowExtraProps={(row, rows) => {
+              return {
+                onClick: (e) => {
+                  console.log("row", row, "rows", rows);
+                  row.toggleSelected();
+                  setSelectedRowData(row.original);
+                  rows.map((item) => {
+                    if (item.getIsSelected() && item.id !== row.id) {
+                      item.toggleSelected();
+                    }
+                  });
+                },
+              };
+            }}
+            // ({
+            //   /////////////???????/////
+            //   // initialState: {()},
+            //   // onLoad: ?????,
+            //   onClick: (e) => {
+            //     console.log('row', row, 'rows', rows);
+            //     row.toggleSelected();
+            //     setSelectedRowData(row.original);
+            //       rows.map((item) => {
+            //         if (item.getIsSelected() && item.id !== row.id) {
+            //           item.toggleSelected();
+            //         }
+            //       });
+            //     },
+            // })}
+          />
+        ) : null}
 
         <div className="urlslab-role-details">
           <EditRole />
@@ -195,28 +210,16 @@ const initialSelectedRow = (rows) => {
               toggleSelectItem={toggleSelectItem}
             />
 
-            <AllCapabilities
-              data={caps || []}
-            />
-
+            <AllCapabilities data={caps || []} />
           </div>
 
-          <div className='urlslab-all-capabilities-footer'>
-            <Button className='urlslab-button' active>
+          <div className="urlslab-all-capabilities-footer">
+            <Button className="urlslab-button" active>
               Save role
             </Button>
           </div>
-
         </div>
-
       </div>
     </>
   );
 }
-
-
-
-
-
-
-
